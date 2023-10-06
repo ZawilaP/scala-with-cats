1d1023d0e6f110e595c37a026038e91bc18e0fb5
DzejDzej <52526807+pzawilaniedzwiecki@users.noreply.github.com>
2023-10-06 13:24:05 +0200

    Added whiteline

--- src/main/scala/Chapter6/Semigroupal.scala
+++ src/main/scala/Chapter6/Semigroupal.scala
@@ -3,6 +3,7 @@ package Chapter6
 import cats.Monad
 import cats.syntax.flatMap._
 import cats.syntax.functor._
+
 object Semigroupal extends App{
   def product[M[_]: Monad, A, B](x: M[A], y: M[B]): M[(A, B)] =
     for {
