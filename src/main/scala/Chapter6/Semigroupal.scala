83fa7634f08208057c3d880c442103ca86eac5d3
zawilaniedzwiecki.p <zawilaniedzwiecki.p@pg.com>
2023-10-06 14:18:30 +0200

    Added exercises from chapter 7

--- src/main/scala/Chapter6/Semigroupal.scala
+++ src/main/scala/Chapter6/Semigroupal.scala
@@ -3,6 +3,7 @@ package Chapter6
 import cats.Monad
 import cats.syntax.flatMap._
 import cats.syntax.functor._
+import scala.language.higherKinds
 
 object Semigroupal extends App{
   def product[M[_]: Monad, A, B](x: M[A], y: M[B]): M[(A, B)] =
