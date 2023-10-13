71c04a6a694641cb66b63bd61b385cac31f8b659
zawilaniedzwiecki.p <zawilaniedzwiecki.p@pg.com>
2023-10-13 15:55:50 +0200

    Added GCounter

--- src/main/scala/Chapter11/CRDT.scala
+++ src/main/scala/Chapter11/CRDT.scala
@@ -1,5 +1,15 @@
 package Chapter11
 
 object CRDT extends App {
+  final case class GCounter(counters: Map[String, Int]) {
+    def increment(machine: String, amount: Int): GCounter =
+      GCounter(counters ++ (machine -> (amount + counters.getOrElse(machine, 0))))
 
+    def merge(that: GCounter): GCounter = GCounter(that.counters ++ this.counters.map {
+      case (k, v) =>
+        k -> (v max that.counters.getOrElse(k, 0))
+    })
+
+    def total: Int = counters.values.sum
+  }
 }
