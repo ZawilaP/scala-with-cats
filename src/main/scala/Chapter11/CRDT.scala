041e71a17ee170d2e95dbeb0bb8076edda83df92
zawilaniedzwiecki.p <zawilaniedzwiecki.p@pg.com>
2023-10-13 16:01:56 +0200

    Added boundedSemiLattice

--- src/main/scala/Chapter11/CRDT.scala
+++ src/main/scala/Chapter11/CRDT.scala
@@ -12,4 +12,26 @@ object CRDT extends App {
 
     def total: Int = counters.values.sum
   }
+
+  import cats.kernel.CommutativeMonoid
+
+  trait BoundedSemiLattice[A] extends CommutativeMonoid[A] {
+    def combine(a1: A, a2: A): A
+
+    def empty: A
+  }
+
+  object BoundedSemiLattice {
+    implicit val intBSL: BoundedSemiLattice[Int] = new BoundedSemiLattice[Int] {
+      def combine(a1: Int, a2: Int): Int = a1 max a2
+
+      def empty: Int = 0
+    }
+
+    implicit def setBSL[A]: BoundedSemiLattice[Set[A]] = new BoundedSemiLattice[Set[A]] {
+      def combine(a1: Set[A], a2: Set[A]): Set[A] = a1 union a2
+
+      def empty: Set[A] = Set.empty[A]
+    }
+  }
 }
